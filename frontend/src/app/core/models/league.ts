2dbd49ff8e7bf190ce8b6b6c87bba7318478b38a
Rafael Pinto <85125753+RafaelPBPinto@users.noreply.github.com>
2026-01-15 15:33:26 +0000

    Change frontend from Angular to Next.js

--- frontend/src/app/core/models/league.ts
+++
@@ -1,7 +0,0 @@
-export interface League {
-    id: number;
-    name: string;
-    country: string;
-    logo: string;
-    season: number;
-}
--- frontend/src/app/core/models/team.ts
+++
@@ -1,8 +0,0 @@
-export interface Team {
-    id: number;
-    name: string;
-    shortName?: string;
-    abbreviation: string;
-    logo: string;
-    country: string;
-}
