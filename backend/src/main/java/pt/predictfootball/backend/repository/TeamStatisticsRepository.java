9ca28f5071c7b3c0a05a2ccd056abbd341070cbf
Rafael Pinto <85125753+RafaelPBPinto@users.noreply.github.com>
2026-02-27 22:21:05 +0000

    Start backend from scratch

--- backend/src/main/java/pt/predictfootball/backend/repository/TeamStatisticsRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.predictfootball.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pt.predictfootball.backend.model.TeamStatistics;
-
-@Repository
-public interface TeamStatisticsRepository extends JpaRepository<TeamStatistics, Long> {
-}
